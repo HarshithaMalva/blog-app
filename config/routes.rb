08c4f91da39d5e13a88cf81e080d7d145230be8f
harshitha <harshum16@gmail.com>
2017-06-10 17:13:50 +0530

    author feature include

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :authors
+
   resources :categories
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330110410) do
+ActiveRecord::Schema.define(version: 20170610113727) do
 
   create_table "article_categories", force: true do |t|
     t.integer  "article_id"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20170330110410) do
     t.text     "body"
     t.boolean  "published"
     t.date     "published_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "authors", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
